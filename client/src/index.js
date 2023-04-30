efe57e61e1edab33fb6823e5ef14b7467a576341
Andrew Walters <andrewjpwalters@gmail.com>
2023-04-30 04:08:28 -0500

    import bootstrap, browserrouter and userprovider into index.js, wrap App in browserrouter and userprovider tags

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/user';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
