7ff2c46d569a6bfaf0ae37ab80a6beb0ea4af4a9
Andrew Walters <andrewjpwalters@gmail.com>
2023-04-30 03:20:49 -0500

    define custom routes, move message resources into conversation resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :messages
-  resources :conversations
+  resources :conversations do
+    resources :messages, only: [:index, :create]
+  end
   resources :sales_posts
   resources :users
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#me"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
