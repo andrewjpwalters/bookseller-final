953ce292ba195b101490a592acaf5c3585411993
Andrew Walters <andrewjpwalters@gmail.com>
2023-05-02 20:56:24 -0500

    create initial UpdateProfileForm component and import to Profile component

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react"
 import { UserContext } from "./context/user"
 import { useParams } from "react-router-dom"
+import UpdateProfileForm from "./UpdateProfileForm"
 
 function Profile() {
     const [profileUser, setProfileUser] = useState(null)
@@ -19,7 +20,11 @@ function Profile() {
             {profileUser ? (
                 <>
                     {profileUser.id === user.id ? (
-                        <h1>Welcome, {profileUser.username}</h1>
+                        <>
+                            <h1>Welcome, {profileUser.username}</h1>
+
+                            <UpdateProfileForm />
+                        </>
                     ) : (
                         <h1>{profileUser.username}</h1>
                     )}
---
+++ client/src/UpdateProfileForm.js
@@ -0,0 +1,7 @@
+function UpdateProfileForm() {
+    return (
+        <h3>Hello from UpdateProfileForm</h3>
+    )
+}
+
+export default UpdateProfileForm
