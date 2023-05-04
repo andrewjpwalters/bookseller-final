74001cc98608b5498e6a81912509c496ac352196
Andrew Walters <andrewjpwalters@gmail.com>
2023-05-03 21:51:46 -0500

    add create and index to conversations and messages controllers

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,27 @@
 class ConversationsController < ApplicationController
+    def index
+        conversation = @current_user.conversations
+        render json: conversation
+    end
+
+    def create
+        conversation = Conversation.new(
+            sender_id: @current_user.id,
+            recipient_id: params[:recipient_id],
+            sales_post_id: params[:sales_post_id]
+        )
+        if conversation.save
+          message = Message.new(
+            content: params[:content],
+            conversation_id: conversation.id,
+          )
+          if message.save
+            render json: { conversation: conversation, message: message }, status: :created
+          else
+            render json: { errors: message.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { errors: conversation.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+    def index
+        conversation = Conversation.find(params[:conversation_id])
+        messages = conversation.messages
+        render json: messages
+    end
+
+    def create
+        conversation = Conversation.find(params[:conversation_id])
+        message = conversation.messages.build(message_params)
+        message.user = @current_user
+      
+        if message.save
+            render json: message, status: :ok
+        else
+            render json: { errors: message.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def message_params
+        params.permit(:content, :user)
+    end
+
 end
