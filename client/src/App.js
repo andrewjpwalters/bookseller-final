669b4ac2b5fb529c2da7dc5f2c6bc6898e0a0268
Andrew Walters <andrewjpwalters@gmail.com>
2023-05-04 05:47:30 -0500

    add wildcard route to lead to 404 page in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -78,6 +78,9 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
+        <Route path="*">
+          <h1>404 NOT FOUND</h1>
+        </Route>
       </Switch>
     </Container>
   );
